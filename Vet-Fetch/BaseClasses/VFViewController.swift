6c62168161538d526e21a0becac9feabc022daa1
Brian <briancorrea@gmail.com>
2016-07-21 23:08:55 -0400

    Observer for LoggedIn Notification

--- Vet-Fetch/BaseClasses/VFViewController.swift
+++ Vet-Fetch/BaseClasses/VFViewController.swift
@@ -10,9 +10,48 @@ import UIKit
 
 class VFViewController: UIViewController {
 
+    static var currentUser = VFProfile() //shared across the application
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+     
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(self,
+                                       selector: #selector(VFViewController.userLoggedIn(_:)),
+                                       name: Constants.kUserLoggedInNotification,
+                                       object: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("CurrentUser: \(VFViewController.currentUser.id)")
+    }
+    
+    func userLoggedIn(notification: NSNotification){
+        if let user = notification.userInfo!["user"] as? Dictionary<String, AnyObject>{
+            VFViewController.currentUser.populate(user)
+        }
+    }
+    
+    func postLoggedInNotification(currentUser: Dictionary<String, AnyObject>){
+        let notificiation = NSNotification(
+            name: Constants.kUserLoggedInNotification,
+            object: nil,
+            userInfo: ["user": currentUser]
+        )
+        
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.postNotification(notificiation)
     }
     
     func exit(){
