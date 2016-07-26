0708adfa6fd1b1e2c3b75bbc5d84b2258d21afad
Brian <briancorrea@gmail.com>
2016-07-25 21:54:50 -0400

    Configuring CollectionView and ScrollView

--- Vet-Fetch/ViewControllers/VFAccountViewController.swift
+++ Vet-Fetch/ViewControllers/VFAccountViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
-class VFAccountViewController: VFViewController {
+class VFAccountViewController: VFViewController, UICollectionViewDelegate, UICollectionViewDataSource, UIScrollViewDelegate {
+    
+    var collectionView: UICollectionView!
+    var btnsArray = ["Appointments", "Pets", "Medications", "Bills"]
+    var bottomScrollView: UIScrollView!
     
     //MARK: - Lifecycle Methods
-    
     required init?(coder aDecoder: NSCoder){
         super.init(coder: aDecoder)
     }
@@ -23,12 +26,28 @@ class VFAccountViewController: VFViewController {
     }
 
     override func loadView(){
-        
         let frame = UIScreen.mainScreen().bounds
         let view = UIView(frame: frame)
-        view.backgroundColor = UIColor(red: 164/255, green: 185/255, blue: 202/255, alpha: 1)
+        view.backgroundColor = .clearColor()
         
-        print("Current User: \(VFViewController.currentUser.id)")
+        let collectionViewLayout = VFCollectionViewFlowLayout()
+        self.collectionView = UICollectionView(frame: frame, collectionViewLayout: collectionViewLayout)
+        
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
+        self.collectionView.backgroundColor = UIColor(red: 164/255, green: 185/255, blue: 202/255, alpha: 1)
+        
+        self.collectionView.registerClass(VFCollectionViewCell.classForCoder(), forCellWithReuseIdentifier: "cellId")
+        
+        let w = 4*Int(frame.size.width)
+        
+        self.bottomScrollView = UIScrollView(frame: CGRect(x: 0, y: frame.size.width, width: frame.size.width, height: frame.size.width))
+        self.bottomScrollView.contentSize = CGSize(width: w, height: 0)
+        self.bottomScrollView.pagingEnabled = true
+        self.bottomScrollView.delegate = self
+        self.collectionView.addSubview(self.bottomScrollView)
+        
+        view.addSubview(self.collectionView)
         
         self.view = view
     }
@@ -36,22 +55,53 @@ class VFAccountViewController: VFViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    }
-    
-    override func viewWillAppear(animated: Bool) {
-        print("viewDidAppear")
-        
         self.navigationController?.navigationBarHidden = true
     }
     
-    override func userLoggedIn(notification: NSNotification){
-        super.userLoggedIn(notification)
+    //MARK: - CollectionView Delegates
+    
+    func configureCell(cell: VFCollectionViewCell, indexPath :NSIndexPath){
+        let button = self.btnsArray[indexPath.row]
         
-        if(VFAccountViewController.currentUser.id == nil) {
-            return
+        cell.cellLabel.text = button
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return self.btnsArray.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cellId = "cellId"
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(cellId, forIndexPath: indexPath) as! VFCollectionViewCell
+        
+        self.configureCell(cell, indexPath: indexPath)
+        
+        return cell
+    }
+    
+    //MARK: ScrollView Delegate
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView){
+        print("scrollViewDidEndDecelerating: \(scrollView.contentOffset.x)")
+        let offset = scrollView.contentOffset.x
+        
+        if (offset == 0){
+            self.bottomScrollView.backgroundColor = .yellowColor()
         }
         
-        print("VFAccountViewController: userLoggedIn")
+        if (offset == self.view.frame.size.width){
+            self.bottomScrollView.backgroundColor = .orangeColor()
+        }
+        
+        if (offset == self.view.frame.size.width*2){
+            self.bottomScrollView.backgroundColor = .redColor()
+        }
+        if (offset == self.view.frame.size.width*3){
+            self.bottomScrollView.backgroundColor = .blueColor()
+        }
     }
     
     override func didReceiveMemoryWarning() {
---
+++ Vet-Fetch/ViewControllers/VFCollectionViewCell.swift
@@ -0,0 +1,51 @@
+//
+//  VFCollectionViewCell.swift
+//  Vet-Fetch
+//
+//  Created by Brian Correa on 7/25/16.
+//  Copyright © 2016 Milkshake Tech. All rights reserved.
+//
+
+import UIKit
+
+class VFCollectionViewCell: UICollectionViewCell {
+    
+    var imageView: UIImageView!
+    var cellLabel: UILabel!
+    
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        
+        let width = frame.size.width
+        let height = frame.size.height
+        
+        self.imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: width, height: height))
+        self.contentView.addSubview(self.imageView)
+        
+        self.cellLabel = UILabel(frame: CGRect(x: 0, y: 0, width: width, height: height))
+        self.cellLabel.textColor = .whiteColor()
+        self.cellLabel.textAlignment = .Center
+        self.contentView.addSubview(self.cellLabel)
+        
+        let colors = [
+            UIColor.greenColor(),
+            UIColor.redColor(),
+            UIColor.blueColor(),
+            UIColor.orangeColor()
+        ]
+
+        var i = arc4random()
+        i = i % UInt32(colors.count)
+        let color = colors[Int(i)]
+
+        self.imageView.backgroundColor = color
+        self.layer.borderWidth = 0.5
+        self.layer.borderColor = UIColor.whiteColor().CGColor
+        
+    }
+    
+}
