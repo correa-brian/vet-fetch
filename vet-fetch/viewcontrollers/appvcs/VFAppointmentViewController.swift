a7d561f9c0cd878d820e3afd0f8d6c8f96ab9dd5
Brian <briancorrea@gmail.com>
2016-07-25 22:34:58 -0400

    loading iniital Views for collectionView vcs

---
+++ vet-fetch/viewcontrollers/appvcs/VFAppointmentViewController.swift
@@ -0,0 +1,43 @@
+//
+//  VFAppointmentViewController.swift
+//  Vet-Fetch
+//
+//  Created by Brian Correa on 7/25/16.
+//  Copyright © 2016 Milkshake Tech. All rights reserved.
+//
+
+import UIKit
+
+class VFAppointmentViewController: VFViewController {
+    
+    //MARK: - Lifecycle Methods
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?){
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        self.edgesForExtendedLayout = .None
+    }
+    
+    override func loadView(){
+        let frame = UIScreen.mainScreen().bounds
+        let view = UIView(frame: frame)
+        view.backgroundColor = .redColor()
+        
+        self.view = view
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBarHidden = false
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
