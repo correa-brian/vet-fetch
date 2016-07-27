4705ff0c17ff8a55d253c659e941ade076b3bffe
Brian <briancorrea@gmail.com>
2016-07-26 23:55:56 -0400

    Moving to Views directory

--- vet-fetch/views/VFCollectionViewCell.swift
+++ vet-fetch/views/VFCollectionViewCell.swift
@@ -32,10 +32,10 @@ class VFCollectionViewCell: UICollectionViewCell {
         self.contentView.addSubview(self.cellLabel)
         
         let colors = [
-            UIColor.greenColor(),
-            UIColor.redColor(),
-            UIColor.blueColor(),
-            UIColor.orangeColor()
+            UIColor(red: 164/255, green: 185/255, blue: 202/255, alpha: 1),
+            UIColor(red: 255/255, green: 220/255, blue: 204/255, alpha: 1),
+            UIColor(red: 255/255, green: 234/255, blue: 204/255, alpha: 1),
+            UIColor(red: 240/255, green: 192/255, blue: 206/255, alpha: 1)
         ]
         
         var i = arc4random()
