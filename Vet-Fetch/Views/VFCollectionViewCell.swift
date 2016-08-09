880fb1193be0d6ce35717a5d54abca637d1d2173
Brian <briancorrea@gmail.com>
2016-08-09 11:47:38 -0400

    Renaming File

---
+++ Vet-Fetch/Views/VFCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  VFCollectionViewCell.swift
+//  Vet-Fetch
+//
+//  Created by Brian Correa on 8/9/16.
+//  Copyright © 2016 Milkshake Tech. All rights reserved.
+//
+
+import UIKit
+
+class VFCollectionViewCell: UICollectionViewCell {
+    
+}
