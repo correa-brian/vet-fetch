23cad57d62f6d5ed7823551be5c4640fc1ced85f
Brian <briancorrea@gmail.com>
2016-08-09 11:52:03 -0400

    Renaming File Paths

--- Vet-Fetch/ViewControllers/AppVCs/VFAppointmentViewController.swift
+++ Vet-Fetch/ViewControllers/AppVCs/VFAppointmentViewController.swift
@@ -8,28 +8,33 @@
 
 import UIKit
 
-class VFAppointmentViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+class VFAppointmentViewController: VFViewController {
+    
+    //MARK: - Lifecycle Methods
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?){
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
     }
-    */
-
+    
+    override func loadView(){
+        let frame = UIScreen.mainScreen().bounds
+        let view = UIView(frame: frame)
+        view.backgroundColor = .redColor()
+        
+        self.view = view
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBarHidden = false
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
