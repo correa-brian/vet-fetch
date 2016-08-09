fa3c5b12ed191558899e64935cdf40d70e4ff177
Brian <briancorrea@gmail.com>
2016-08-09 11:51:32 -0400

    Renaming Collection View

--- Vet-Fetch/Views/VFCollectionViewFlowLayout.swift
+++ Vet-Fetch/Views/VFCollectionViewFlowLayout.swift
@@ -9,5 +9,20 @@
 import UIKit
 
 class VFCollectionViewFlowLayout: UICollectionViewFlowLayout {
-
+    
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+    }
+    
+    override init(){
+        super.init()
+        
+        let frame = UIScreen.mainScreen().bounds
+        
+        let dimension = frame.size.width / 2
+        self.itemSize = CGSizeMake(dimension, dimension)
+        self.minimumInteritemSpacing = 0 //horizontal gape between columns
+        self.minimumLineSpacing = 0 //vertical gap between rows
+        self.sectionInset = UIEdgeInsetsMake(0.0, 0.0, 0.0, 0.0)
+    }
 }
