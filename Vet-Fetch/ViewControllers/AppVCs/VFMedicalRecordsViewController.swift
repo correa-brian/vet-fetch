6cf6152be19cdace714fe4f8769d2d2c6c8ea100
Brian <briancorrea@gmail.com>
2016-08-22 23:52:38 -0400

    Testing Embedded TableView

--- Vet-Fetch/ViewControllers/AppVCs/VFMedicalRecordsViewController.swift
+++ Vet-Fetch/ViewControllers/AppVCs/VFMedicalRecordsViewController.swift
@@ -42,43 +42,35 @@ class VFMedicalRecordsViewController: VFViewController, UICollectionViewDelegate
             btn.frame = CGRect(x: 0, y: y, width: width, height: height*0.15)
             btn.backgroundColor = .redColor()
             btn.layer.borderWidth = 1.0
-            btn.layer.borderColor = UIColor.yellowColor().CGColor
+            btn.layer.borderColor = UIColor.greenColor().CGColor
             btn.setTitle(titles[i], forState: .Normal)
             btn.tintColor = .whiteColor()
+            btn.addTarget(self, action: #selector(VFMedicalRecordsViewController.moveScroll(_:)), forControlEvents: .TouchUpInside)
             view.addSubview(btn)
+            btnsArray.append(btn)
             y += btn.frame.size.height
         }
         
         let collectionViewLayout = VFMedicalCollectionViewFlowLayout()
-        self.collectionView = UICollectionView(frame: CGRect(x: 0, y: y, width: width, height: height-y), collectionViewLayout: collectionViewLayout)
+        self.collectionView = UICollectionView(frame: CGRect(x: 0, y: y, width: width, height: height-(y+49)), collectionViewLayout: collectionViewLayout)
         self.collectionView.dataSource = self
         self.collectionView.delegate = self
         self.collectionView.registerClass(VFMedicalCollectionViewCell.classForCoder(), forCellWithReuseIdentifier: "cellId")
-        self.collectionView.pagingEnabled = true
+        self.collectionView.pagingEnabled = false
         self.collectionView.backgroundColor = .yellowColor()
+        self.collectionView.showsHorizontalScrollIndicator = false
         
         view.addSubview(self.collectionView)
-        
-//        self.optionsTable = UITableView(frame: CGRect(x: 0, y: 0, width: width, height: height*0.5), style: .Plain)
-//        self.optionsTable.delegate = self
-//        self.optionsTable.dataSource = self
-//        self.optionsTable.registerClass(UITableViewCell.classForCoder(), forCellReuseIdentifier: "cellId")
-//        
-//        view.addSubview(self.optionsTable)
         
         self.view = view
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        print("Height: \(self.collectionView.frame.size.height)")
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        
-        print("viewWillAppear")
         
         self.tabBarController?.navigationItem.rightBarButtonItem = nil
         self.navigationController?.navigationBar.barTintColor = .clearColor()
@@ -87,14 +79,48 @@ class VFMedicalRecordsViewController: VFViewController, UICollectionViewDelegate
         self.navigationController?.navigationBar.shadowImage = nil
     }
     
+    func moveScroll(btn: UIButton){
+        print("moveScroll: \(btn.titleLabel?.text)")
+        
+        var bool = false
+        
+        if btn.titleLabel?.text == "Medication"{
+            bool = true
+            self.collectionView.scrollToItemAtIndexPath(NSIndexPath(forItem: 0, inSection: 0), atScrollPosition: .CenteredHorizontally, animated: bool)
+        }
+        
+        if btn.titleLabel?.text == "Vaccines"{
+            bool = true
+            self.collectionView.scrollToItemAtIndexPath(NSIndexPath(forItem: 1, inSection: 0), atScrollPosition: .CenteredHorizontally, animated: bool)
+        }
+        
+        if btn.titleLabel?.text == "Allergies"{
+            bool = true
+            self.collectionView.scrollToItemAtIndexPath(NSIndexPath(forItem: 2, inSection: 0), atScrollPosition: .CenteredHorizontally, animated: bool)
+        }
+        
+        if bool == true {
+            UIView.animateWithDuration(0.75,
+                                       animations: {
+                                        
+                                        for item in self.btnsArray{
+                                            item.backgroundColor = .redColor()
+                                        }
+                                        
+                                        btn.backgroundColor = .purpleColor()
+            })
+        }
+        
+    }
+    
+    
     //MARK: - CollectionView Delegates
     func configureCell(cell: VFMedicalCollectionViewCell, indexPath :NSIndexPath){
-        
-        let labels = ["Number 1", "Number 2", "Number 3"]
-        
-        let text = labels[indexPath.row]
-        
-        cell.cellLabel.text = text
+//        
+//        let labels = ["Medication", "Vaccines", "Allergies"]
+//        
+//        let text = labels[indexPath.row]
+//        cell.cellLabel.text = text
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -103,26 +129,23 @@ class VFMedicalRecordsViewController: VFViewController, UICollectionViewDelegate
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         
-        let cellId = "cellId"
+//        let cellId = "cellId"
         
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(cellId, forIndexPath: indexPath) as! VFMedicalCollectionViewCell
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(VFMedicalCollectionViewCell.cellId, forIndexPath: indexPath) as! VFMedicalCollectionViewCell
         
-        self.configureCell(cell, indexPath: indexPath)
-        
+        print("Cell: \(indexPath.row)")
+//        self.configureCell(cell, indexPath: indexPath)
+//        cell.cellLabel.text = ("\(indexPath.row)")
         return cell
     }
     
-//    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 3
-//    }
-//    
-//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-//        
-//        let cell = tableView.dequeueReusableCellWithIdentifier("cellId", forIndexPath: indexPath)
-//        cell.textLabel?.text = "\(indexPath.row)"
-//        return cell
-//        
-//    }
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        
+        let width = self.collectionView.frame.size.width
+        let height = self.collectionView.frame.size.height
+    
+        return CGSize(width: width, height: height)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
