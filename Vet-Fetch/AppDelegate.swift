173ef4bd403848976c03439be8f5febe0a7071cc
Brian <briancorrea@gmail.com>
2016-09-10 21:25:05 -0400

    Printing sandbox path

--- Vet-Fetch/AppDelegate.swift
+++ Vet-Fetch/AppDelegate.swift
@@ -16,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
+        print(NSHomeDirectory())
+        
+        let directories = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+        
+        if let documents = directories.first {
+            print(documents)
+        }
+        
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
         
         let welcomeVc = VFWelcomeViewController()
