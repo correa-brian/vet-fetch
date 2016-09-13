ad9e7c70c4cb546739cb3113d8331d127b6772d1
Brian <briancorrea@gmail.com>
2016-09-12 21:34:12 -0400

    Checking keychain for userId to login

--- Vet-Fetch/AppDelegate.swift
+++ Vet-Fetch/AppDelegate.swift
@@ -15,45 +15,73 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        let userIdKey = KeychainWrapper.defaultKeychainWrapper().stringForKey("userId")
         
-        print(NSHomeDirectory())
-        
-        let directories = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
-        
-        if let documents = directories.first {
-            print(documents)
+        if userIdKey != nil{
+            print("id retrieved")
+            let accountVc = VFAccountViewController()
+            let accountNavCtr = UINavigationController(rootViewController: accountVc)
+            self.window?.rootViewController = accountNavCtr
+        }
+            
+        else{
+            print("id not retrieved")
+            let welcomeVc = VFWelcomeViewController()
+            let welcomeNavCtr = UINavigationController(rootViewController: welcomeVc)
+            self.window?.rootViewController = welcomeNavCtr
         }
         
-        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        
-        let welcomeVc = VFWelcomeViewController()
-        let welcomeNavCtr = UINavigationController(rootViewController: welcomeVc)
-        self.window?.rootViewController = welcomeNavCtr
-        
+        self.userLogin()
         self.window?.makeKeyAndVisible()
-
-        self.checkCurrentUser()
         return true
     }
     
-    func checkCurrentUser(){
+    func userLogin(){
+        print("checkCurrentUser")
         
-        APIManager.checkCurrentUser { response in
-            if let currentUserInfo = response["user"] as? Dictionary<String, AnyObject>{
+        let retrievedEmail = KeychainWrapper.defaultKeychainWrapper().stringForKey("profileEmail")
+        let retrievedPassword = KeychainWrapper.defaultKeychainWrapper().stringForKey("profilePassword")
+        
+        var profileInfo = Dictionary<String,AnyObject>()
+        profileInfo["email"] = retrievedEmail
+        profileInfo["password"] = retrievedPassword
+        
+        APIManager.postRequest("/account/login", params: profileInfo, completion: { error, response in
+            
+//            if error != nil {
+//                let errorObj = error?.userInfo
+//                let errorMsg = errorObj!["message"] as! String
+//                
+//                dispatch_async(dispatch_get_main_queue(), {
+//                    let alert = UIAlertController(title: "Message", message: errorMsg, preferredStyle: .Alert)
+//                    alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+//                    
+//                    self.presentViewController(alert, animated: true, completion: nil)
+//                })
+//                return
+//            }
+
+            if let result = response!["currentUser"] as? Dictionary<String, AnyObject>{
                 
                 let currentUser = VFProfile()
-                currentUser.populate(currentUserInfo)
-            
-                let notification = NSNotification(
-                    name: Constants.kUserLoggedInNotification,
-                    object: nil,
-                    userInfo: ["user":currentUserInfo]
-                )
+                currentUser.populate(result)
                 
-                let notificationCenter = NSNotificationCenter.defaultCenter()
-                notificationCenter.postNotification(notification)                
+                KeychainWrapper.defaultKeychainWrapper().setString(currentUser.id!, forKey: "userId")
+
+                dispatch_async(dispatch_get_main_queue(), {
+                    let notification = NSNotification(
+                        name: Constants.kUserLoggedInNotification,
+                        object: nil,
+                        userInfo: ["user":result]
+                    )
+
+                    let notificationCenter = NSNotificationCenter.defaultCenter()
+                    notificationCenter.postNotification(notification)
+                })
             }
-        }
+        })
     }
 
     func applicationWillResignActive(application: UIApplication) {
