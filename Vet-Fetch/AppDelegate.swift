9561093eb0407e9ae7230d6d30eab946ab81afea
Brian <briancorrea@gmail.com>
2016-09-14 23:40:44 -0400

    Fetching pets and logging user in

--- Vet-Fetch/AppDelegate.swift
+++ Vet-Fetch/AppDelegate.swift
@@ -13,34 +13,30 @@ import Alamofire
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    let userIdKey = KeychainWrapper.defaultKeychainWrapper().stringForKey("userId")
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        let userIdKey = KeychainWrapper.defaultKeychainWrapper().stringForKey("userId")
         
-        if userIdKey != nil{
-            print("id retrieved")
+        if self.userIdKey != nil{
             let accountVc = VFAccountViewController()
             let accountNavCtr = UINavigationController(rootViewController: accountVc)
             self.window?.rootViewController = accountNavCtr
-        }
-            
-        else{
-            print("id not retrieved")
+        } else {
             let welcomeVc = VFWelcomeViewController()
             let welcomeNavCtr = UINavigationController(rootViewController: welcomeVc)
             self.window?.rootViewController = welcomeNavCtr
         }
-        
         self.userLogin()
         self.window?.makeKeyAndVisible()
         return true
     }
     
+    func applicationDidBecomeActive(application: UIApplication) {
+        self.fetchPets()
+    }
+    
     func userLogin(){
-        print("checkCurrentUser")
-        
         let retrievedEmail = KeychainWrapper.defaultKeychainWrapper().stringForKey("profileEmail")
         let retrievedPassword = KeychainWrapper.defaultKeychainWrapper().stringForKey("profilePassword")
         
@@ -49,59 +45,58 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         profileInfo["password"] = retrievedPassword
         
         APIManager.postRequest("/account/login", params: profileInfo, completion: { error, response in
-            
-//            if error != nil {
-//                let errorObj = error?.userInfo
-//                let errorMsg = errorObj!["message"] as! String
-//                
-//                dispatch_async(dispatch_get_main_queue(), {
-//                    let alert = UIAlertController(title: "Message", message: errorMsg, preferredStyle: .Alert)
-//                    alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
-//                    
-//                    self.presentViewController(alert, animated: true, completion: nil)
-//                })
-//                return
-//            }
-
+            if error != nil {
+                return
+            }
             if let result = response!["currentUser"] as? Dictionary<String, AnyObject>{
-                
                 let currentUser = VFProfile()
                 currentUser.populate(result)
-                
                 KeychainWrapper.defaultKeychainWrapper().setString(currentUser.id!, forKey: "userId")
-
+                
                 dispatch_async(dispatch_get_main_queue(), {
                     let notification = NSNotification(
                         name: Constants.kUserLoggedInNotification,
                         object: nil,
                         userInfo: ["user":result]
                     )
-
                     let notificationCenter = NSNotificationCenter.defaultCenter()
                     notificationCenter.postNotification(notification)
                 })
             }
         })
     }
+    
+    func fetchPets(){
+        if self.userIdKey == nil {
+            return
+        }
+        var petInfo = Dictionary<String, AnyObject>()
+        petInfo["ownerId"] = self.userIdKey
+        
+        APIManager.getRequest("/api/pet", params: petInfo, completion: { response in
+            if let results = response["results"] as? Array<Dictionary<String, AnyObject>>{
+                if VFViewController.pets.count != results.count{
+                    VFViewController.pets.removeAll()
+                    for result in results {
+                        let pet = VFPet()
+                        pet.populate(result)
+                        VFViewController.pets.append(pet)
+                    }
+                }
+            }
+        })
+    }
 
     func applicationWillResignActive(application: UIApplication) {
-
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
-        
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
-        
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        
     }
 
     func applicationWillTerminate(application: UIApplication) {
- 
     }
 
 }
